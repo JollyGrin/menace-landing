29650a4227d3f545be6be6ca2103214806507bf2
JollyGrin <lol@dean.lol>
2025-05-16 00:09:16 +0200

    making progress

---
+++ src/lib/HeroTiltCard.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import VanillaTilt from 'vanilla-tilt';
+	import { onMount } from 'svelte';
+
+	let {
+		src = '/people/George_Washington.png',
+		title = 'George Washington'
+	}: { src: string; title: string } = $props();
+
+	let tiltRef: HTMLElement | null = $state(null);
+	onMount(() => {
+		if (tiltRef) {
+			VanillaTilt.init(tiltRef, {
+				max: 25,
+				speed: 400,
+				glare: true,
+				transition: true,
+				'max-glare': 0.5
+			});
+		}
+	});
+</script>
+
+<img
+	bind:this={tiltRef}
+	alt={title}
+	{title}
+	{src}
+	class="my-3 w-24 rounded-xl shadow-md transition-all duration-500 select-none hover:shadow-amber-200/10 hover:saturate-150 focus:scale-110 active:scale-110 md:w-30"
+/>
--- src/routes/MarqueeSection.svelte
+++ src/routes/MarqueeSection.svelte
@@ -1,17 +1,27 @@
 <script lang="ts">
+	import VanillaTilt from 'vanilla-tilt';
 	import Marquee from '$lib/Marquee.svelte';
+	import { onMount } from 'svelte';
+	import HeroTiltCard from '$lib/HeroTiltCard.svelte';
+
+	let tiltRef: HTMLElement | null = $state(null);
+	onMount(() => {
+		if (tiltRef) {
+			VanillaTilt.init(tiltRef, {
+				max: 25,
+				speed: 400,
+				glare: true,
+				transition: true,
+				'max-glare': 0.5
+			});
+		}
+	});
 </script>
 
-<Marquee pauseOnHover>
-	<img alt="hero" src="/people/George_Washington.png" />
-	<img alt="hero" src="/people/Joan_of_Arc.png" />
+<Marquee numberOfCopies={10} pauseOnHover>
+	<HeroTiltCard src="/people/George_Washington.png" title="George Washington" />
+	<HeroTiltCard src="/people/Emmeline_Pankhurst.png" title="Emmeline Pankhurst" />
 </Marquee>
-
-<style>
-	:global(.marq-group) {
-		& img {
-			width: 5rem;
-			border-radius: 0.75rem;
-		}
-	}
-</style>
+<Marquee numberOfCopies={10} pauseOnHover reverse>
+	<HeroTiltCard />
+</Marquee>
