049d6a696e801f47b558fee02527efeedb01c4eb
JollyGrin <lol@dean.lol>
2025-05-15 23:18:13 +0200

    add quote

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,7 @@
 <script lang="ts">
-	import { Canvas } from '@threlte/core';
-	import Scene from './Scene.svelte';
-	import { AsciiRenderer } from '@threlte/extras';
-	import { type AsciiEffectOptions } from 'three/examples/jsm/Addons.js';
-
-	let fgColor = $state('#3cdbd3');
-	let bgColor = $state('#000000');
-	const defaultCharacters = ' .:-+*=%@#';
-	let characters = $state(defaultCharacters);
-	let alpha = $state(1);
-	let block = $state(false);
-	let color = $state(false);
-	let invert = $state(false);
-	let resolution = $state(0.2);
-	let scale = $state(1);
-	const options: AsciiEffectOptions = $derived({
-		alpha,
-		block,
-		color,
-		invert,
-		resolution,
-		scale
-	});
+	import Quote from '$lib/Quote.svelte';
 </script>
 
-<div class="h-screen w-screen bg-black">
-	<Canvas>
-		<Scene />
-		<AsciiRenderer {bgColor} {characters} {fgColor} {options} />
-	</Canvas>
-</div>
+<img src="/logo.png" alt="logo" class="mx-auto w-[150px] md:w-[200px]" />
+
+<Quote />
